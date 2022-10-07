605258afdedbcb27c287618d4dfc0731b3469fc7
Damian Slocombe <wooshiidev@gmail.com>
2022-10-07 18:09:48 +0100

    Renamed mineplex-request to mineplex-stats

--- src/mineplex-stats.js
+++ src/mineplex-stats.js
@@ -1,31 +1,39 @@
-const Mineplex = require('./api/mp-fetch');
+// noinspection JSUnusedGlobalSymbols
 
-export class Request
-{
-    // Leaderboards
+const keys = require("./api/mp-keys");
+const {Mineplex} = require("./api/mp-fetch");
+
+class Stats {
+
+    // --- Leaderboards
 
     /**
      * Get the leaderboards for wins in the given game.
-     * @param {LB_GAMES | String} game - The game mode.
+     * @param {GAMES | String} game - The game mode.
      * @param {BOARDS | String} board - The leaderboard duration type.
      * @returns {Promise<null|Leaderboard>} - Returns the fetched leaderboard for wins.
      * Can return null if a leaderboard cannot be found for the given parameters.
      */
-    async GetWinLeaderboard(game, board)
+    static async getWinLeaderboard(game, board)
     {
-        return await Mineplex.GetLeaderboard(game, "Wins", board);
+        return await Mineplex.getLeaderboard(game, "Wins", board);
     }
+
+    // --- Endpoints
 
     /**
      * Get the leaderboards for a particular stat in the given game.
-     * @param {LB_GAMES | String} game - The game mode.
+     * @param {GAMES | String} game - The game mode.
      * @param {String} stat - The stat the leaderboard is for.
      * @param {BOARDS | String} board - The leaderboard duration type.
      * @returns {Promise<null|Leaderboard>} - Returns the fetched leaderboard.
      * Can return null if a leaderboard cannot be found for the given parameters.
      */
-    async GetLeaderboard(game, stat, board)
+    static async getLeaderboard(game, stat, board)
     {
-        return await Mineplex.GetLeaderboard(game, stat, board);
+        return await Mineplex.getLeaderboard(game, stat, board);
     }
 }
+
+const stats = new Stats();
+module.exports = stats;
